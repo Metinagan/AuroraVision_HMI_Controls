46b51da45c5a13a90cf49bfac70754787609334c
Metin Agan <metin.agan01@gmail.com>
2025-05-13 09:14:47 +0300

    update RAdialProgressBar color settings

--- AuroraVision_Controls/Controls.cs
+++ AuroraVision_Controls/Controls.cs
@@ -35,6 +35,7 @@ namespace AuroraVision_Controls
 
 
 
+
            
             
            
--- AuroraVision_Controls/progressBarRadial.cs
+++ AuroraVision_Controls/progressBarRadial.cs
@@ -21,12 +21,28 @@ namespace AuroraVision_Controls
         public int _textSize = 14;
         public Color _textColor = Color.Black;
 
+        public Color _progressBaseColor = Color.DimGray;
+
+        public int _value1 = 20;
+        public Color _value1Color = Color.Red;
+        public int _value2 = 35;
+        public Color _value2Color = Color.Orange;
+        public int _value3 = 50;
+        public Color _value3Color = Color.Yellow;
+        public int _value4 = 70;
+        public Color _value4Color = Color.YellowGreen;
+        public int _value5 = 85;
+        public Color _value5Color = Color.Green;
+        public Color _value6Color = Color.GreenYellow;
+
         public progressBarRadial()
         {
             InitializeComponent();
             this.Resize += ProgressBarRadial_Resize;
             CenterLabel();
             label1.ForeColor = _textColor;
+            label1.BackColor = Color.Transparent;
+            radialProgressBar1.ProgressBaseColor = _progressBaseColor;
         }
 
         [HMIBrowsable(true)]
@@ -59,29 +75,213 @@ namespace AuroraVision_Controls
             {
                 _progressValue = value;
                 label1.Text = value.ToString("0.##") + "%";
+                CenterLabel();
 
                 radialProgressBar1.Value = value;
-                if (value <= 20)
+                if (value <= _value1)
                 {
                     radialProgressBar1.ColorAnimationCycle.Clear();
-                    radialProgressBar1.ColorAnimationCycle.Add(Color.Red);
+                    radialProgressBar1.ColorAnimationCycle.Add(_value1Color);
                 }
-                else if (value > 20 && value <= 80)
+                else if (value > _value1 && value <= _value2)
                 {
                     radialProgressBar1.ColorAnimationCycle.Clear();
-                    radialProgressBar1.ColorAnimationCycle.Add(Color.Orange);
+                    radialProgressBar1.ColorAnimationCycle.Add(_value2Color);
                 }
-                else if (value > 80)
+                else if (value > _value2 && value<=_value3)
                 {
                     radialProgressBar1.ColorAnimationCycle.Clear();
-                    radialProgressBar1.ColorAnimationCycle.Add(Color.Green);
+                    radialProgressBar1.ColorAnimationCycle.Add(Value3Color);
+                }
+                else if (value > _value3 && value <= _value4)
+                {
+                    radialProgressBar1.ColorAnimationCycle.Clear();
+                    radialProgressBar1.ColorAnimationCycle.Add(_value4Color);
+                }
+                else if (value > _value4 && value <= _value5)
+                {
+                    radialProgressBar1.ColorAnimationCycle.Clear();
+                    radialProgressBar1.ColorAnimationCycle.Add(_value5Color);
+                }
+                else
+                {
+                    radialProgressBar1.ColorAnimationCycle.Clear();
+                    radialProgressBar1.ColorAnimationCycle.Add(_value6Color);
                 }
             }
         }
 
         [HMIBrowsable(true)]
         [Browsable(true)]
-        [Category("Colors Settings")]
+        [Category("Color Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 1 Color")]
+        public Color Value1Color
+        {
+            get { return _value1Color; }
+            set
+            {
+                _value1Color = value;
+                radialProgressBar1.ColorAnimationCycle.Clear();
+                radialProgressBar1.ColorAnimationCycle.Add(_value1Color);
+            }
+        }
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Color Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 2 Color")]
+        public Color Value2Color
+        {
+            get { return _value2Color; }
+            set
+            {
+                _value2Color = value;
+                radialProgressBar1.ColorAnimationCycle.Clear();
+                radialProgressBar1.ColorAnimationCycle.Add(_value2Color);
+            }
+        }
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Color Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 3 Color")]
+        public Color Value3Color
+        {
+            get { return _value3Color; }
+            set
+            {
+                _value3Color = value;
+                radialProgressBar1.ColorAnimationCycle.Clear();
+                radialProgressBar1.ColorAnimationCycle.Add(_value3Color);
+            }
+        }
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Color Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 4 Color")]
+        public Color Value4Color
+        {
+            get { return _value4Color; }
+            set
+            {
+                _value4Color = value;
+                radialProgressBar1.ColorAnimationCycle.Clear();
+                radialProgressBar1.ColorAnimationCycle.Add(_value4Color);
+            }
+        }
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Color Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 5 Color")]
+        public Color Value5Color
+        {
+            get { return _value5Color; }
+            set
+            {
+                _value5Color = value;
+                radialProgressBar1.ColorAnimationCycle.Clear();
+                radialProgressBar1.ColorAnimationCycle.Add(_value5Color);
+            }
+        }
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Color Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 6 Color")]
+        public Color Value6Color
+        {
+            get { return _value6Color; }
+            set
+            {
+                _value6Color = value;
+                radialProgressBar1.ColorAnimationCycle.Clear();
+                radialProgressBar1.ColorAnimationCycle.Add(_value6Color);
+            }
+        }
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Value Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 1")]
+        public int Value1
+        {
+            get { return _value1; }
+            set
+            {
+                _value1 = value;
+            }
+        }
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Value Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 2")]
+        public int Value2
+        {
+            get { return _value2; }
+            set
+            {
+                _value2 = value;
+            }
+        }
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Value Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 3")]
+        public int Value3
+        {
+            get { return _value3; }
+            set
+            {
+                _value3 = value;
+            }
+        }
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Value Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 4")]
+        public int Value4
+        {
+            get { return _value4; }
+            set
+            {
+                _value4 = value;
+            }
+        }
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Value Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Value 5")]
+        public int Value5
+        {
+            get { return _value5; }
+            set
+            {
+                _value5 = value;
+            }
+        }
+
+
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Color Settings")]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
         [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
         [Description("Changes Background Color")]
@@ -97,7 +297,7 @@ namespace AuroraVision_Controls
         }
         [HMIBrowsable(true)]
         [Browsable(true)]
-        [Category("Colors Settings")]
+        [Category("ColorsSettings")]
         [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
         [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
         [Description("Changes Text Fore Color")]
@@ -142,5 +342,23 @@ namespace AuroraVision_Controls
                 );
             }
         }
+
+        [HMIBrowsable(true)]
+        [Browsable(true)]
+        [Category("Color Settings")]
+        [DesignerSerializationVisibility(DesignerSerializationVisibility.Visible)]
+        [HMI.HMIPortProperty(HMIPortDirection.HiddenInput)]
+        [Description("Changes Progress Bar Base Color")]
+        public Color ProgressBaseColor
+        {
+            get { return _progressBaseColor; }
+            set
+            {
+                _progressBaseColor = value;
+                radialProgressBar1.ProgressBaseColor = value;
+            }
+        }
+
+
     }
 }
